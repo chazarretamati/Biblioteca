24cd6345b690404bcc6d9f0d9768a14f11b21fb0
Nicolas Daniel Roman Moreno <danielrm963@gmail.com>
2025-11-16 17:08:09 -0300

    Funciones; Agregar y Listar libros

--- src/servicios/BibliotecaService.java
+++ src/servicios/BibliotecaService.java
@@ -1,5 +1,44 @@
 package servicios;
 
+
+import modelos.libro;   
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class BibliotecaService {
-    
+
+    private ArrayList<libro> libros = new ArrayList<>();  //Lista para almacenar libros
+
+    public void agregarLibro(Scanner sc) {
+        System.out.print("Código del libro: ");
+        int codigo = sc.nextInt();
+        sc.nextLine();
+
+        System.out.print("Título: ");
+        String titulo = sc.nextLine();
+
+        System.out.print("Autor: ");
+        String autor = sc.nextLine();
+
+        System.out.print("Stock: ");
+        int stock = sc.nextInt();
+        sc.nextLine();
+
+        libro libroNuevo = new libro(codigo, stock, titulo, autor); 
+        libros.add(libroNuevo);
+
+        System.out.println(" Libro agregado correctamente.\n");
+    }
+
+    public void listarLibros() {
+        if (libros.isEmpty()) {
+            System.out.println(" No hay libros registrados.\n");
+            return;
+        }
+
+        System.out.println("\n===== LISTADO DE LIBROS =====");
+        libros.forEach(System.out::println);
+        System.out.println();
+    }
 }
+

4fa6c27f2955c9bf68022ec93e69f468112f7941
Nicolas Daniel Roman Moreno <danielrm963@gmail.com>
2025-11-16 16:27:00 -0300

    Agregar la clase ´libro´ con atributos, contructores, y metodos basicos

--- src/modelos/libro.java
+++ src/modelos/libro.java
@@ -1,4 +1,53 @@
 package modelos;
 public class libro {
     
+    //Atributos
+    private int codigo, stock;
+    private String titulo, autor;
+
+    //Constructor
+    public libro(int codigo, int stock, String titulo, String autor){
+        this.codigo = codigo;
+        this.stock = stock;
+        this.titulo = titulo;
+        this.autor = autor;
+    }
+
+    //Getters y Setters
+    public int getCodigo(){
+        return codigo;
+    }
+
+    public void setCodigo(int codigo){
+        this.codigo = codigo;
+    }
+
+    public int getStock(){
+        return stock;
+    }
+
+    public void setStock(int stock){
+        this.stock = stock;
+    }
+
+    public String getTitulo(){
+        return titulo;
+    }
+
+    public void setTitutlo(String titulo){
+        this.titulo = titulo;
+    }
+
+    public String getAutor(){
+        return autor;
+    }
+
+    public void setAutor(String autor){
+        this.autor = autor;
+    }
+
+    @Override
+    public String toString(){//Metodo para mostrar la informacion del libro con todo lo implementado
+        return "["+codigo+"]"+titulo+" - "+autor+" (Stock: "+stock+")";
+    }
 }

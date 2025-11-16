20089e1712988e6234bc5fb4ad37cf1b8f47b733
Nicolas Daniel Roman Moreno <danielrm963@gmail.com>
2025-11-16 17:26:23 -0300

    Funciones; Agregar y Listar libros

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+import servicios.BibliotecaService;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        BibliotecaService servicio = new BibliotecaService();
+
+        int opcion;
+
+        do {
+            System.out.println("===== SISTEMA DE BIBLIOTECA =====");
+            System.out.println("1. Agregar libro");
+            System.out.println("2. Listar libros");
+            System.out.println("3. Salir");
+            System.out.print("Seleccione una opción: ");
+            opcion = sc.nextInt();
+            sc.nextLine();
+
+            switch (opcion) {
+                case 1 -> servicio.agregarLibro(sc);
+                case 2 -> servicio.listarLibros();
+                case 3 -> System.out.println("Saliendo....");
+                default -> System.out.println("Opción inválida\n");
+            }
+
+        } while (opcion != 3);
+    }
+}
--- src/modelos/libro.java
+++ src/modelos/libro.java
@@ -1,4 +1,5 @@
 package modelos;
+
 public class libro {
     
     //Atributos
